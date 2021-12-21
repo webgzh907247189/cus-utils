7aae86782eddb6a60caa5f3f4b2a4d3eb0e34267
gzh <18255106634@163.com>
2021-12-21 19:20:21 +0800

    添加gitignore文件

--- types/awaitWrap.d.ts
+++ types/awaitWrap.d.ts
@@ -1,1 +1,1 @@
-export default function awaitWrap<T, U = any>(promise: Promise<T>): Promise<[U | null, T | null]>;
+export default function awaitWrap<T, U = unknown>(promise: Promise<T>): Promise<[U | null, T | null]>;
