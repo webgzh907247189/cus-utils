3a62447e40924995df404600425820b11bed42dc
gzh <18255106634@163.com>
2022-09-18 23:32:24 +0800

    feat： add parseUrlQuery test

--- test/parseUrlQuery.test.ts
+++ test/parseUrlQuery.test.ts
@@ -7,7 +7,7 @@ describe('测试 parseUrlQuery 文件', () => {
         expect(data.a).toEqual(['1', '4', '5']);
     });
 
-    it('测试 parseUrlQuery 传入空字符串', async () => {
+    it('测试 parseUrlQuery 传入字符串,返回key 带有数组的情况', async () => {
         const data = parseUrlQuery('a=1&b=2&c=3&a=4&a=5');
         expect(data.b).toEqual('2');
     });
@@ -15,5 +15,10 @@ describe('测试 parseUrlQuery 文件', () => {
     it('测试 parseUrlQuery 传入空字符串', async () => {
         const data = parseUrlQuery('');
         expect(data).toEqual({});
+    });
+
+    it('测试 parseUrlQuery 传入带?的字符串', async () => {
+        const data = parseUrlQuery('?a=1&b=2&c=3');
+        expect(data).toEqual({a: '1', b: '2', c: '3'});
     });
 });
