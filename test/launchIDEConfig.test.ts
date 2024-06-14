0f7c29a134e126b90ad9c1aa5d118483c9a04bb0
jonas.ge <jonas.ge@jonas2ge.local>
2024-06-14 17:36:23 +0800

    feat: new version

--- test/launchIDEConfig.test.ts
+++ test/launchIDEConfig.test.ts
@@ -3,8 +3,19 @@ import { jsStr } from '../src/launchIDEConfig'
 
 describe('测试 launchIDEConfig 文件', () => {
 
-    it('测试 launchIDEConfig 存入 返回值', () => {
+    it('测试 launchIDEConfig 存入 返回值 没有参数的情况', () => {
         const result = launchIDEConfig();
         expect(result).toEqual(jsStr);
+    });   
+
+    it('测试 launchIDEConfig 存入 返回值 有参数的情况', () => {
+        const ideName = 'vscode';
+        const result = launchIDEConfig(ideName);
+        const str = `(() => {
+            let ideName = '${ideName}';
+            let options = { ideName: ideName };
+            ${jsStr};
+        })()`;
+        expect(result).toEqual(str);
     });   
 });
