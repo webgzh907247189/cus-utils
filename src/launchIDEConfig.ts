f7c1cdf310d9bdbe8e707231bc893f6fc04ac640
jonas.ge <jonas.ge@jonas2ge.local>
2024-06-14 17:12:33 +0800

    feat: new version

--- src/launchIDEConfig.ts
+++ src/launchIDEConfig.ts
@@ -84,7 +84,14 @@ export const jsStr = `(function(options) {
         document.addEventListener('dblclick', documentClickEvent);
     }
     console.log('%c%s', 'color: red;font-size: 28px;background: #fff;', '已成功开启 launch IDE 功能');
-})(options)`;
-export default function launchIDEConfig() {
+})(options ?? {})`;
+export default function launchIDEConfig(ideName?: string) {
+    if (ideName) {
+        return `(() => {
+            let ideName = '${ideName}';
+            let options = { ideName: ideName };
+            ${jsStr};
+        })()`;
+    }
     return jsStr;
 }
