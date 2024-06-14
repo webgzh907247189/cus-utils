794cab6b42e369e1cc4966636666e880a823977f
jonas.ge <jonas.ge@jonas2ge.local>
2024-06-14 18:13:42 +0800

    feat: new version

--- src/launchIDEConfig.ts
+++ src/launchIDEConfig.ts
@@ -86,14 +86,7 @@ export const jsStr = `(function(options) {
     console.log('%c%s', 'color: red;font-size: 28px;background: #fff;', '已成功开启 launch IDE 功能');
 })(options ?? {})`;
 export default function launchIDEConfig(ideName?: string) {
-    return ideName
-        ? `(() => {
-        let ideName = '${ideName}';
-        let options = { ideName: ideName };
-        ${jsStr};
-    })()`
-        : `(() => {
-        let options = {};
-        ${jsStr};
-    })()`;
+    const jsStr1 = `(()=>{let ideName = '${ideName}';let options = { ideName: ideName };${jsStr};})()`;
+    const jsStr2 = `(()=>{let options = {};${jsStr};})()`;
+    return ideName ? jsStr1 : jsStr2;
 }
--- test/launchIDEConfig.test.ts
+++ test/launchIDEConfig.test.ts
@@ -5,17 +5,14 @@ describe('测试 launchIDEConfig 文件', () => {
 
     it('测试 launchIDEConfig 存入 返回值 没有参数的情况', () => {
         const result = launchIDEConfig();
-        expect(result).toEqual(jsStr);
+        const jsStr1 = `(()=>{let options = {};${jsStr};})()`;
+        expect(result).toEqual(jsStr1);
     });   
 
     it('测试 launchIDEConfig 存入 返回值 有参数的情况', () => {
         const ideName = 'vscode';
         const result = launchIDEConfig(ideName);
-        const str = `(() => {
-            let ideName = '${ideName}';
-            let options = { ideName: ideName };
-            ${jsStr};
-        })()`;
+        const str =`(()=>{let ideName = '${ideName}';let options = { ideName: ideName };${jsStr};})()`;
         expect(result).toEqual(str);
     });   
 });
