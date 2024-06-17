0f8f59c08e1ae2344ebb9c3b2f51b5b6c372a79a
jonas.ge <jonas.ge@jonas2ge.local>
2024-06-17 22:33:37 +0800

    feat: add new version

--- src/launchIDEConfig.ts
+++ src/launchIDEConfig.ts
@@ -1,90 +1,56 @@
-export const jsStr = `(function(options) {
-    const ideName = options.ideName ?? 'vscode';
-    const SKIP = 'SKiP';
-    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+export const jsStr = `(function (options) {
+    const ideName = options.ideName ?? "vscode";
+    const SKIP = "SKiP";
 
     const documentClickEvent = (event) => {
-        let completeFilepath = event.target.getAttribute('complete-filepath');
-        let currentElement = event.target;
-        let parentElement;
+      let completeFilepath = event.target.getAttribute("complete-filepath");
+      let currentElement = event.target;
+      let parentElement;
 
-        while (completeFilepath === null) {
-            if (completeFilepath === SKIP) {
-                return;
-            }
-            const nodeName = currentElement.nodeName.toLowerCase();
-            // console.log(nodeName, "nodeNamenodeName");
+      while (completeFilepath === null) {
+        if (completeFilepath === SKIP) {
+          return;
+        }
+        const nodeName = currentElement.nodeName.toLowerCase();
+        // console.log(nodeName, "nodeNamenodeName");
 
-            if (nodeName === 'body') {
-                throw new Error('请检查 babel-plugin-jsxfileattribute 插件是否开启');
-            }
-
-            parentElement = currentElement.parentElement;
-
-            completeFilepath = parentElement.getAttribute('complete-filepath');
-
-            currentElement = parentElement;
+        if (nodeName === "body") {
+          throw new Error("请检查 babel-plugin-jsxfileattribute 插件是否开启");
         }
 
-        if (completeFilepath !== SKIP) {
-            // console.log(completeFilepath, 'completeFilepath');
+        parentElement = currentElement.parentElement;
 
-            window.location.assign(ideName + '://file' + completeFilepath);
-        }
+        completeFilepath = parentElement.getAttribute("complete-filepath");
+
+        currentElement = parentElement;
+      }
+
+      if (completeFilepath !== SKIP && window.ISCLICK) {
+        console.log(completeFilepath, "completeFilepath");
+
+        window.location.assign(ideName + "://file" + completeFilepath);
+        window.ISCLICK = false;
+      }
     };
 
-    if (isMobile) {
-        let fragementElement = document.createElement('div');
-        // 样式设置
-        const styleObj = {
-            width: '200px',
-            height: '80px',
-            color: 'green',
-            backgroundColor: 'red',
-            position: 'fixed',
-            borderRadius: '44px',
-            top: '0px',
-            fontSize: '16px',
-            left: '0px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            zIndex: '9999',
-        };
+    document.addEventListener("click", documentClickEvent);
 
-        for (const key in styleObj) {
-            const element = styleObj[key];
-            fragementElement.style[key] = styleObj[key];
-        }
-        fragementElement.setAttribute('complete-filepath', SKIP);
-
-        fragementElement.appendChild(document.createTextNode('点击我  开启唤起 vscode 开关'));
-
-        fragementElement.addEventListener(
-            'click',
-            () => {
-                // 取 反
-                window.ISMOBILECLICK = !window.ISMOBILECLICK;
-            },
-            true,
-        );
-
-        document.addEventListener(
-            'click',
-            (event) => {
-                if (window.ISMOBILECLICK) {
-                    documentClickEvent(event);
-                }
-            },
-            true,
-        );
-
-        document.body.appendChild(fragementElement);
-    } else {
-        document.addEventListener('dblclick', documentClickEvent);
-    }
-    console.log('%c%s', 'color: red;font-size: 28px;background: #fff;', '已成功开启 launch IDE 功能');
-})(options ?? {})`;
+    const ALT = "Alt";
+    window.addEventListener("keydown", (event) => {
+      if (event.altKey) {
+        window.ISCLICK = true;
+      }
+    });
+    window.addEventListener("keyup", (event) => {
+      window.ISCLICK = false;
+    });
+    console.log(
+      "%c%s",
+      "color: red;font-size: 28px;background: #fff;",
+      "已成功开启 launch IDE 功能",
+    );
+  })(options ?? {});
+`;
 export default function launchIDEConfig(ideName?: string) {
     const jsStr1 = `(()=>{let ideName = '${ideName}';let options = { ideName: ideName };${jsStr};})()`;
     const jsStr2 = `(()=>{let options = {};${jsStr};})()`;
